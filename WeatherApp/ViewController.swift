98ae390673e1fcef2ac7ba8ae2d734aeba1651d9
Andrey Krivlenya <drus@MacBook-Air-Andrey.local>
2021-12-29 12:38:37 +0300

    init

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Andrey on 29.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
